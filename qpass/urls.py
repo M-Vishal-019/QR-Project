1cec9ae75f0fb465104aa47f11dc370470be5e82
M-Sai-Kumar3538 <saik62263@gmail.com>
2026-03-13 11:45:44 +0530

    updated backend

--- qpass/urls.py
+++ qpass/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('student_login/',views.student_login,name='student_login'),
     path('security_login/',views.security_login,name='security_login'),
-    path('student_login/student/',views.student,name='student'),
-    path('security_login/security/',views.security,name='security'),
+    path('student_login/student/',views.student_dashboard,name='student'),
+    path('security_login/security/',views.security_dashboard,name='security'),
     path('student_login/student/student_history/',views.student_history,name='student_history'),
-    path('security_login/security/lateList/',views.lateList,name='security'),
+    path('security_login/security/lateList/',views.late_list,name='security'),
     path('student_login/student/logout/',views.logout,name='logout'),
 
 ]
