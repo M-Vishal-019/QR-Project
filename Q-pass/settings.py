6a02336202576748ac099b3a12e8aaf998c9a31c
M-Vishal-019 <vishalmadharaboyina750@gmail.com>
2026-03-29 18:13:13 +0530

    Remove duplicate staticfiles

--- Q-pass/settings.py
+++ Q-pass/settings.py
@@ -35,13 +35,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'qpass',
