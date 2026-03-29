b4f397d1423e15e1e1bb40712b388adeb9862ca3
M-Vishal-019 <vishalmadharaboyina750@gmail.com>
2026-03-29 18:55:47 +0530

    Revert core module name back to firstproject

--- firstproject/wsgi.py
+++ firstproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Q-pass.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
 
 application = get_wsgi_application()
